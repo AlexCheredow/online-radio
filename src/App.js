9e7c945c2f588efb0f4aba70be591b1d2a0900ac
AlexCheredow <yasha.cheredow@yandex.ru>
2021-10-11 19:47:49 +0300

    started-work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import RadioInfo from "./components/RadioInfo/RadioInfo";
+import classes from "./App.module.css";
+import img1 from "./img/1.png";
+import img2 from "./img/2.png";
+import img3 from "./img/3.png";
+import img4 from "./img/4.png";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div className={classes.wrapper}>
+      <RadioInfo img={img1} />
+      <RadioInfo img={img2} />
+      <RadioInfo img={img3} />
+      <RadioInfo img={img4} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/RadioInfo/RadioInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./RadioInfo.module.css";
+
+const RadioInfo = (img) => {
+  return (
+    <div className={classes.bg}>
+      <h3>название станции</h3>
+    </div>
+  );
+};
+export default RadioInfo;
